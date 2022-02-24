98beb1fc42b49d748f3e6932109e0e604029be98
Evgeniy <jon83@inbox.ru>
2022-02-24 21:20:06 +0300

    Zadacha_2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Проверить квадрат
+Console.Write("Введите число 1 - ");
+int a = Convert.ToInt32(Console.ReadLine()); // Конвертируем строку в целое число
+// Вещественное число double и ToDouble у Convert
+Console.Write("Введите число 2 - ");
+int b = Convert.ToInt32(Console.ReadLine()); // Конвертируем строку в целое число
+if (a == b * b)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
