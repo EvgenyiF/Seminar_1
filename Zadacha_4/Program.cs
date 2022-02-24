c9765c19d3f3c5270a2f90f3ca4b65e0ca92df88
Evgeniy <jon83@inbox.ru>
2022-02-24 21:55:32 +0300

    Zadacha_4

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать числа от -N до N
+Console.Write(" Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+for (int x = a; x <= b; x++ )
+{
+    Console.Write( x );
+    Console.Write(" ");
+
+}
