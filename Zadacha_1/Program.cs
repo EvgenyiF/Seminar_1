9afcd2e765b0fe5dbd140798ec6185a0cc70db03
Evgeniy <jon83@inbox.ru>
2022-02-24 20:57:44 +0300

    Zadacha_1_v.1

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Вывести квадрат числа
 Console.Write("Введите число - ");
 int numberValue = Convert.ToInt32(Console.ReadLine()); // Конвертируем строку в целое число
+// Вещественное число double и ToDouble у Convert
 int resaltValue = numberValue * numberValue;
 Console.Write("Квадрат числа равен ");
 Console.WriteLine(resaltValue);
